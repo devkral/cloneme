7cbb596c4569f719db7e69bdd131885c86cea0d5
alex <devkral@web.de>
2012-08-01 16:33:38 +0200

    update TODO(TODO,install-installer-compiler),make lilo stub

--- src/share/sh/install-installer-compiler.sh
+++ src/share/sh/install-installer-compiler.sh
@@ -10,10 +10,15 @@ sharedir="$(dirname "$(dirname "$(realpath "$0")")")"
 architecture="$1"
 output="$(realpath "$2")"
 
-
-
 if [ "$(uname -m)" != "$architecture" ];then
-  #cd "$sharedir/src"
-  #gcc -o "$output" main.cc `pkg-config --libs --cflags vte-2.90,gtkmm-3.0`
-  return 2
+  cd "$sharedir/src"
+  #TODO: add missing options so it works 
+  if ! gcc -o "$output" `pkg-config --libs --cflags vte-2.90 gtkmm-3.0` -std=gnu++11 -Wall -DPACKAGE_DATA_DIR="\"${sharedir}\"" ./main.cc; then
+    
+    exit 1
+  fi
+  
+  exit 2
 fi
+
+exit 0
